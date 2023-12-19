7ee3ffacbe9fb8d63d519af5ffccf40a2c6b85b5
asdfwoomin <154343478+asdfwoomin@users.noreply.github.com>
2023-12-19 23:56:40 +0900

    Add files via upload

---
+++ src/test/java/com/study/PostServiceTest.java
@@ -0,0 +1,47 @@
+package com.study;
+
+import com.study.domain.post.PostRequest;
+import com.study.domain.post.PostService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    PostService postService;
+
+    /*@Test
+    void save() {
+        PostRequest params = new PostRequest();
+        params.setTitle("1번 게시글 제목");
+        params.setContent("1번 게시글 내용");
+        params.setWriter("테스터");
+        params.setNoticeYn(false);
+        Long id = postService.savePost(params);
+        System.out.println("생성된 게시글 ID : " + id);
+    }*/
+    /*@Test
+    void updatePost(){
+        PostRequest params = new PostRequest();
+        params.setId(3L);
+        params.setTitle("1번 게시글 제목 수정합니다.");
+        params.setContent("1번 게시글 내용 수정합니다222222.");
+        params.setWriter("도뎡이223");
+        params.setNoticeYn(true);
+        postService.updatePost(params);
+    }*/
+
+    @Test
+    void saveByForeach() {
+        for (int i = 1; i <= 1000; i++) {
+            PostRequest params = new PostRequest();
+            params.setTitle(i + "번 게시글 제목");
+            params.setContent(i + "번 게시글 내용");
+            params.setWriter("테스터" + i);
+            params.setNoticeYn(false);
+            postService.savePost(params);
+        }
+    }
+}
